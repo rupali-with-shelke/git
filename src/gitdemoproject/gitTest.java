cd7e42413e8fac760cc31b49380d2cd7f507a41b
vilas-with-shelke <vilas1160shelke@gmail.com>
2023-05-18 22:25:00 +0530

    first commit....

---
+++ src/gitdemoproject/gitTest.java
@@ -0,0 +1,16 @@
+package gitdemoproject;
+
+public class gitTest {
+
+	public static void main(String[] args) {
+		String hungry = "yes";
+		if(hungry =="yes") {
+			System.out.println("eat something...");
+		}
+		else
+		{
+			System.out.println("Do work....");
+		}
+	}
+
+}
